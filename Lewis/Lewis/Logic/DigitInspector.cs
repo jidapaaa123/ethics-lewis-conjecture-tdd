6f3416f23b6f38203cff5561c7cc709054fdcfaf
Jidapa <jidapaang5011@gmail.com>
2026-01-22 02:10:49 -0700

    digitinspector failing tests

---
+++ Lewis/Lewis/Logic/DigitInspector.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lewis.Logic
+{
+    public class DigitInspector
+    {
+        public int Number { get; set; }
+        public DigitInspector(int number)
+        {
+            if (number < 0)
+            {
+                throw new ArgumentException("Only positive integers are accepted.", nameof(number));
+            }
+
+            Number = number;
+        }
+
+        public int DigitSum()
+        {
+            string numStr = Number.ToString();
+            foreach (char c in numStr)
+            {
+                // Intentionally left incomplete for testing purposes
+            }
+            
+            throw new NotImplementedException();
+        }
+
+        public int DigitRoot()
+        {
+            string digitSumStr = DigitSum().ToString();
+
+            while (digitSumStr.Length > 1)
+            {
+                // Intentionally left incomplete for testing purposes
+            }
+
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ Lewis/TestProject1/DigitInspectorTests.cs
@@ -0,0 +1,33 @@
+﻿using Lewis.Logic;
+using Shouldly;
+
+namespace TestProject1
+{
+    public class DigitInspectorTests
+    {
+        [Fact]
+        public void DigitSum_Correct()
+        {
+            var di = new DigitInspector(12345);
+            var result = di.DigitSum();
+            result.ShouldBe(15);
+        }
+
+        [Fact]
+        public void DigitRoot_Correct()
+        {
+            var di = new DigitInspector(12345);
+            var result = di.DigitRoot();
+            result.ShouldBe(6);
+        }
+
+        [Fact]
+        public void DigitInspector_OnlyAccepts_PositiveIntegrs()
+        {
+            Should.Throw<ArgumentException>(() =>
+            {
+                var di = new DigitInspector(-5);
+            });
+        }
+    }
+}
