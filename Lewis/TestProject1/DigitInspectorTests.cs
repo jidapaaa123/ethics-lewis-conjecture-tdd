b8b8fe4b4f55410f3afd5da59f5080d94290def5
Jidapa <jidapaang5011@gmail.com>
2026-01-22 03:07:30 -0700

    parameterized tests applied to digit inspector

--- Lewis/TestProject1/DigitInspectorTests.cs
+++ Lewis/TestProject1/DigitInspectorTests.cs
@@ -5,45 +5,59 @@ namespace TestProject1
 {
     public class DigitInspectorTests
     {
-        [Fact]
-        public void DigitSum_Correct()
+        [Theory]
+        [InlineData(12345, 15)]
+        public void DigitSum_Correct(long num, long expected)
         {
-            var di = new DigitInspector(12345);
+            var di = new DigitInspector(num);
             var result = di.DigitSum();
-            result.ShouldBe(15);
+            result.ShouldBe(expected);
         }
 
-        [Fact]
-        public void DigitRoot_Correct_From2Digit_DigitSum()
+        [Theory]
+        [InlineData(12345, 6)]
+        public void DigitRoot_Correct_From2Digit_DigitSum(long num, long expected)
         {
-            var di = new DigitInspector(12345);
+            var di = new DigitInspector(num);
             var result = di.DigitRoot();
-            result.ShouldBe(6);
+            result.ShouldBe(expected);
         }
 
-        [Fact]
-        public void Correct_DigitSum_Has_3Digits()
+        [Theory]
+        [InlineData("999999999999", 108)]
+        public void Correct_DigitSum_Has_3Digits(string numStr, long expected)
         {
-            // 9*12 = 108, so make a number that's just 9, 12 times
-            var di = new DigitInspector(long.Parse("999999999999"));
+            var di = new DigitInspector(long.Parse(numStr));
             var result = di.DigitSum();
-            result.ShouldBe(108);
+            result.ShouldBe(expected);
 
         }
-        [Fact]
-        public void DigitRoot_Correct_From3Digit_DigitSum()
+
+        [Theory]
+        [InlineData("999999999999", 9)]
+        public void DigitRoot_Correct_From3Digit_DigitSum(string numStr, long expected)
         {
-            var di = new DigitInspector(long.Parse("999999999999"));
+            var di = new DigitInspector(long.Parse(numStr));
             var result = di.DigitRoot();
-            result.ShouldBe(9);
+            result.ShouldBe(expected);
         }
 
-        [Fact]
-        public void DigitInspector_OnlyAccepts_PositiveIntegrs()
+        [Theory]
+        [InlineData(-12345)]
+        [InlineData(-4)]
+        [InlineData(-3)]
+        [InlineData(-2)]
+        [InlineData(-1)]
+        public void DigitInspector_OnlyAccepts_PositiveIntegrs(long negative)
         {
             Should.Throw<ArgumentException>(() =>
             {
-                var di = new DigitInspector(-5);
+                var di = new DigitInspector(negative);
+            });
+            Should.NotThrow(() =>
+            {
+                var di = new DigitInspector(0);
+                var di2 = new DigitInspector(12345);
             });
         }
     }
