0cff253588e932ee60aef3b2674842d929c5a857
Jidapa <jidapaang5011@gmail.com>
2026-01-22 02:51:58 -0700

    lewis conjecure: true for 1st million numbers

---
+++ Lewis/TestProject1/LewisConjectureTests.cs
@@ -0,0 +1,37 @@
+﻿using Lewis.Logic;
+using Shouldly;
+
+namespace TestProject1
+{
+    public class LewisConjectureTests
+    {
+        [Fact]
+        public void LewisConjecture_HoldsForFirstMillionNumbers()
+        {
+            const int limit = 1_000_000;
+            for (int n = 1; n <= limit; n++)
+            {
+                HashSet<string> permutationStrings = new Scrambler(n).GetAllPermutationStrings();
+                if (permutationStrings.Count == 0)
+                {
+                    continue;
+                }
+
+                List<long> absoluteDifferences = permutationStrings
+                        .Select(str => long.Parse(str))
+                        .Where(scr => scr != n)
+                        .Select(scr => Math.Abs(scr - n))
+                        .ToList();
+
+                List<long> digitRootsOfDifferences = absoluteDifferences
+                        .Select(diff => new DigitInspector(diff).DigitRoot())
+                        .ToList();
+
+                bool anyNonNineRoots = digitRootsOfDifferences.Any(root => root != 9);
+
+                anyNonNineRoots.ShouldBeFalse($"Lewis Conjecture failed for number {n}");
+            }
+        }
+
+    }
+}
