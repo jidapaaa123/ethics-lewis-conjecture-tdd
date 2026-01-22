e8de91e2c9bfae9f05215bafa8e65e00623a2ba2
Jidapa <jidapaang5011@gmail.com>
2026-01-22 01:33:43 -0700

    failing scrambler tes

---
+++ Lewis/Lewis/Logic/Scrambler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lewis.Logic
+{
+    public class Scrambler
+    {
+        public int Number { get; set; }
+        public List<int> GetAllPermutations()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Scrambler(int number)
+        {
+            Number = number;
+        }
+    }
+}
---
+++ Lewis/TestProject1/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using Lewis.Logic;
+using Shouldly;
+
+namespace TestProject1
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Scrambler_AllPermutations()
+        {
+            int og = 123;
+            Scrambler scrambler = new Scrambler(og);
+
+            var expectedPermutations = new List<int> { 123, 132, 213, 231, 312, 321 };
+
+            scrambler.GetAllPermutations().ShouldBe(expectedPermutations);
+        }
+    }
+}
